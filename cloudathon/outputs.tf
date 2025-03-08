26852130a2465d54fb62122f34a7bd6b56c61ce7
Venkat <dasarivenkat463@gmail.com>
2025-03-08 20:46:36 +0530

    Workflow Run Test

--- cloudathon/outputs.tf
+++ cloudathon/outputs.tf
@@ -1,10 +1,8 @@
 # Output the instance details
 output "instance_details" {
   value = {
-    self_link    = data.google_compute_instance.testserver.self_link
     machine_type = data.google_compute_instance.testserver.machine_type
     network_ip   = data.google_compute_instance.testserver.network_interface[0].network_ip
-    status       = data.google_compute_instance.testserver.status
   }
   description = "Details of the testserver instance"
 }
