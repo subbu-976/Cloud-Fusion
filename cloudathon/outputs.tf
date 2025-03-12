16035265970f85dd92c5fa9ed144d44d9d46351c
Venkat <dasarivenkat463@gmail.com>
2025-03-12 20:53:22 +0530

    Destroying cluster and DB

--- cloudathon/outputs.tf
+++ cloudathon/outputs.tf
@@ -1,26 +1,26 @@
-# cloudathon/outputs.tf
+# # cloudathon/outputs.tf
 
-output "active_cluster_endpoint" {
-  value       = google_container_cluster.active_cluster.endpoint
-  description = "Endpoint for the active GKE cluster in asia-south2"
-}
+# output "active_cluster_endpoint" {
+#   value       = google_container_cluster.active_cluster.endpoint
+#   description = "Endpoint for the active GKE cluster in asia-south2"
+# }
 
-output "passive_cluster_endpoint" {
-  value       = google_container_cluster.passive_cluster.endpoint
-  description = "Endpoint for the passive GKE cluster in asia-south1"
-}
+# output "passive_cluster_endpoint" {
+#   value       = google_container_cluster.passive_cluster.endpoint
+#   description = "Endpoint for the passive GKE cluster in asia-south1"
+# }
 
-output "postgres_primary_ip" {
-  value       = google_sql_database_instance.postgres_primary.ip_address[0].ip_address
-  description = "IP address of the PostgreSQL primary instance in asia-south2"
-}
+# output "postgres_primary_ip" {
+#   value       = google_sql_database_instance.postgres_primary.ip_address[0].ip_address
+#   description = "IP address of the PostgreSQL primary instance in asia-south2"
+# }
 
-output "postgres_replica_ip" {
-  value       = google_sql_database_instance.postgres_replica.ip_address[0].ip_address
-  description = "IP address of the PostgreSQL replica instance in asia-south1"
-}
+# output "postgres_replica_ip" {
+#   value       = google_sql_database_instance.postgres_replica.ip_address[0].ip_address
+#   description = "IP address of the PostgreSQL replica instance in asia-south1"
+# }
 
-output "load_balancer_ip" {
-  value       = google_compute_global_address.global_ip.address
-  description = "Global load balancer IP for GKE clusters"
-}
+# output "load_balancer_ip" {
+#   value       = google_compute_global_address.global_ip.address
+#   description = "Global load balancer IP for GKE clusters"
+# }
